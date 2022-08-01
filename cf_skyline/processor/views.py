c7b9a2741967fd2815df12249383310c4964a9f5
Temirbek <atabekov.t.a@gmail.com>
2022-08-01 22:26:03 +0600

    modified:   cf_skyline/cfskyline/settings.py 	modified:   cf_skyline/processor/urls.py 	modified:   cf_skyline/processor/views.py 	modified:   cf_skyline/stl_builder/_text2stl.py 	deleted:    cf_skyline/stl_builder/base.stl 	new file:   cf_skyline/stl_builder/cf-base.stl 	new file:   cf_skyline/stl_builder/empty.stl 	new file:   cf_skyline/stl_builder/scad_files/2022_aa02df82cd50413fa282dce18595d362.scad 	new file:   cf_skyline/stl_builder/scad_files/muratmurat_4eec0dbc137e4bd5923c4e8fdc98b5a7.scad 	new file:   cf_skyline/stl_builder/stl_unassembled/2022_bba796a3cad34eb3a47d211c49723c00.stl 	new file:   cf_skyline/stl_builder/stl_unassembled/murat.stl 	renamed:    cf_skyline/stl_builder/text.stl -> cf_skyline/stl_builder/stl_unassembled/muratmurat.stl 	new file:   cf_skyline/stl_builder/stl_unassembled/muratmurat_676582019a724ce3942751c0902ef301.stl 	new file:   cf_skyline/stl_builder/stl_unassembled/year.stl 	deleted:    cf_skyline/stl_builder/text.png 	deleted:    cf_skyline/stl_builder/text.scad

--- cf_skyline/processor/views.py
+++ cf_skyline/processor/views.py
@@ -6,7 +6,9 @@ from parser import CalendarParser
 from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 import json
 import asyncio
-
+import stl_builder._text2stl as converter   
+from cfskyline.settings import MEDIA_ROOT
+import os
 
 @api_view(['GET'])
 def ActivityDict(request):
@@ -15,6 +17,26 @@ def ActivityDict(request):
         data = asyncio.run(CalendarParser.user_activity_dict(username))
         if (data!=None):
             return Response(data, status=HTTP_200_OK)
+        else:
+            return Response('Invalid username', status=HTTP_400_BAD_REQUEST)
+    else:
+        return Response('Invalid username', status=HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def STLLink(request):
+    if (request.data.get('username') and request.data.get('year')):
+        username = request.data.get('username')
+        year = request.data.get('year')
+        data = asyncio.run(CalendarParser.user_activity_dict(username))
+        if (data!=None):
+            username_file_name = converter.Converter._generate_text(username)
+            year_file_name = converter.Converter._generate_year(year)
+            # towers script
+            jsn = {
+                "name" : os.path.join(MEDIA_ROOT, username_file_name),
+                "year" : os.path.join(MEDIA_ROOT, year_file_name),
+            }
+            return Response(jsn, status=HTTP_200_OK)
         else:
             return Response('Invalid username', status=HTTP_400_BAD_REQUEST)
     else:
