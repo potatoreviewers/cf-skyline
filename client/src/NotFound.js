145b4acd49c7a2392f2a09a36923703a4da0c518
muratsat <muratsat@users.noreply.github.com>
2022-08-03 18:57:03 +0600

    navigation in 404 page

--- client/src/NotFound.js
+++ client/src/NotFound.js
@@ -1,8 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 
 function NotFound() {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  }
+
   return (
-      <div className="NotFound">
-        <h1> Not found </h1>
+      <div className="NotFound" id="notfound-page">
+        <div id ="notfound-content">
+          <h1>404</h1>
+          <h2>Page not found</h2>
+          <p>The page you are looking for does not exist.</p>
+          <button onClick={handleClick}>Go to home page</button>
+        </div>
       </div>
   );
 }
