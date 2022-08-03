4e477009bbe52c42ae74c95093b5d2e251659378
muratsat <muratsat@users.noreply.github.com>
2022-08-03 11:42:35 +0600

    implementet api fetching in user page

--- client/src/user/UserPage.js
+++ client/src/user/UserPage.js
@@ -1,12 +1,38 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function UserPage() {
     const {username, year} = useParams();
-    // console.log(username, year);
+
+    const [message, setMessage] = useState('');
+    const [userpic, setUserpic] = useState();
+
+    useEffect(
+      () => {
+        fetch(`https://codeforces.com/api/user.info?handles=${username}`)
+          .then(response => response.json())
+          .then(data => {
+            if (data.status === 'FAILED') {
+              setMessage(data.comment);
+            }
+            else {
+              setUserpic(data.result[0].titlePhoto);
+              setMessage(`${username}'s activity in ${year}`);
+            }
+          }
+        );
+      }, 
+      [username, userpic]
+    );
+
+
 
   return (
       <div className="UserPage">
-        <h1> Hello {username} {year} </h1>
+        <h1> Codeforces skylines </h1>
+        {userpic && <img src={userpic} alt="userpic" id="userpic" />}
+        <p> {message} </p>
       </div>
   );
 }
