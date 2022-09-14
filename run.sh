e3783fa0f97586b516f80850119734cf129da3a2
muratsat <muratsat@users.noreply.github.com>
2022-09-14 10:51:52 +0300

    added run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# This script is used to run the program
+mkdir -p ./cf_skyline/stl_builder/stl_unassembled/
+mkdir -p ./cf_skyline/stl_builder/stl_unassembled/assembled/
+
+.env/bin/pip3 install -r ./cf_skyline/requirements.txt
+
+.env/bin/python3 cf_skyline/manage.py runserver
