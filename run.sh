0911438325ea8907f7a761ed4d5a47f64aa473e9
muratsat <muratsat@users.noreply.github.com>
2022-09-14 10:52:30 +0300

    create venv in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 mkdir -p ./cf_skyline/stl_builder/stl_unassembled/
 mkdir -p ./cf_skyline/stl_builder/stl_unassembled/assembled/
 
+python3 -m venv .env
+
 .env/bin/pip3 install -r ./cf_skyline/requirements.txt
 
 .env/bin/python3 cf_skyline/manage.py runserver
