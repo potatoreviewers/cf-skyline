62c55eaaf02f2fe4b220cf2bb869e83b82f6e260
muratsat <muratsat@users.noreply.github.com>
2022-09-25 09:41:20 +0300

    remove backend

--- cf_skyline/processor/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import ActivityDict, STLLink
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('activity/', ActivityDict, name='activity'),
-    path('stl/', STLLink, name='activity'),
-]
